632e39f996f78205a415c804a5d19bf8d42e9a46
y0n1 <y0n1@outlook.com>
2017-07-11 01:30:19 +0300

    refactor: DRYies the current code

--- src/Shelves.js
+++ src/Shelves.js
@@ -9,20 +9,18 @@ class Shelves extends Component {
 
   componentWillReceiveProps(receivedProps) {
     const onShelf = (shelfName) => (book) => book.shelf === shelfName;
-    const {books} = receivedProps
-    this.setState({ currentlyReading: books.filter(onShelf('currentlyReading')) })
-    this.setState({ wantToRead: books.filter(onShelf('wantToRead')) })
-    this.setState({ read: books.filter(onShelf('read')) })
+    const { books } = receivedProps
+    Object.keys(this.state).forEach((shelfName) => this.setState({ [shelfName]: books.filter(onShelf(shelfName)) }))
   }
 
   render() {
     return (
       <div className="Shelves">
         <ul>
-          <li>Currently Reading<hr/>
+          <li>Currently Reading<hr />
             {this.state.currentlyReading.map(book => (
               <figure key={book.id}>
-                <img src={book.imageLinks.thumbnail} alt=""/>
+                <img src={book.imageLinks.thumbnail} alt="" />
                 <figcaption>
                   <h5>{book.title}</h5>
                   <h6>{book.authors.join(', ')}</h6>
@@ -30,27 +28,27 @@ class Shelves extends Component {
               </figure>
             ))}
           </li>
-          <li>Want to Read<hr/>
+          <li>Want to Read<hr />
             {this.state.wantToRead.map(book => (
-                <figure key={book.id}>
-                  <img src={book.imageLinks.thumbnail} alt=""/>
-                  <figcaption>
-                    <h5>{book.title}</h5>
-                    <h6>{book.authors.join(', ')}</h6>
-                  </figcaption>
-                </figure>
-              ))}
+              <figure key={book.id}>
+                <img src={book.imageLinks.thumbnail} alt="" />
+                <figcaption>
+                  <h5>{book.title}</h5>
+                  <h6>{book.authors.join(', ')}</h6>
+                </figcaption>
+              </figure>
+            ))}
           </li>
-          <li>Read<hr/>
+          <li>Read<hr />
             {this.state.read.map(book => (
-                <figure key={book.id}>
-                  <img src={book.imageLinks.thumbnail} alt=""/>
-                  <figcaption>
-                    <h5>{book.title}</h5>
-                    <h6>{book.authors.join(', ')}</h6>
-                  </figcaption>
-                </figure>
-              ))}
+              <figure key={book.id}>
+                <img src={book.imageLinks.thumbnail} alt="" />
+                <figcaption>
+                  <h5>{book.title}</h5>
+                  <h6>{book.authors.join(', ')}</h6>
+                </figcaption>
+              </figure>
+            ))}
           </li>
         </ul>
       </div>
