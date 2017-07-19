f83ade394dd65d308f3fcec00ceacc82e924c7f7
y0n1 <y0n1@outlook.com>
2017-07-20 01:34:46 +0300

    fix: Fixes typo and sanity checks response properties

--- src/Book.js
+++ src/Book.js
@@ -8,12 +8,12 @@ function Book(props) {
   return (
     <figure className="Book">
       <div className="Book-thumbnail-and-selector-container">
-        <img className="Book-thumbnail" src={book.imageLinks.thumbnail} alt={`${book.title} - ${book.subtitile}`} />
+        <img className="Book-thumbnail" src={book.imageLinks.thumbnail} alt={`${book.title} - ${book.subtitle && book.subtitle}`} />
         <ShelfSelector bookID={book.id} selectedShelf={book.shelf} onChangeShelf={props.onChangeShelf} />
       </div>
       <figcaption>
         <h4>{book.title}</h4>
-        <h5>{book.authors.join(', ')}</h5>
+        <h5>{book.authors && book.authors.join(', ')}</h5>
       </figcaption>
     </figure>
   )
